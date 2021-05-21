1474318b04b92e54553a9bea2d3f31bb24269213
Linjun <linjunlee96@gmail.com>
2021-05-20 23:51:36 -0700

    Init commit

---
+++ src/main/java/com/fisher/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.fisher.service;
+
+import com.spring.Component;
+
+@Component("orderService")
+public class OrderService {
+}
---
+++ src/main/java/com/fisher/service/XxUtil.java
@@ -0,0 +1,4 @@
+package com.fisher.service;
+
+public class XxUtil {
+}
